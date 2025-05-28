09288561b66cc20a51be492072bdfa159f3586e3
HoangDuc1091 <htduc.btdb1@gmail.com>
2025-05-28 23:32:57 +0700

    tinh dien tich

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter width: ");
+        int width = int.Parse(Console.ReadLine());  
+        Console.Write("Enter height: ");
+        int height = int.Parse(Console.ReadLine());
+        int area = width * height;
+        Console.WriteLine("area: "+ area);
+    }
+}
